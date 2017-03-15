49b9b76d846e7f541a733ee93e12c492da1887a8
Luke <luke@digitalbridge.com.au>
2017-03-15 16:06:48 +1100

    PHASE 2: Consistency of naming of buttons through the plugin – e.g. cancel in some places, dismiss in others CANCEL is the preference

--- apps/crate_it/templates/help_modal.php
+++ apps/crate_it/templates/help_modal.php
@@ -18,7 +18,7 @@
       </div>
 
       <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Dismiss</button>
+        <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
       </div>
     </div>
   </div>
--- apps/crate_it/templates/publish_confirm_modal.php
+++ apps/crate_it/templates/publish_confirm_modal.php
@@ -21,7 +21,7 @@
         </p>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Dismiss</button>
+        <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
       </div>
     </div><!-- /.modal-content -->
   </div><!-- /.modal-dialog -->
