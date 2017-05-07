0aa29fb523299c25fa5d6d37a4483d46a1aa334d
Luke <luke@digitalbridge.com.au>
2017-05-07 17:40:41 +1000

    Read Email Settings from Config: crate_to_email_fallback, crate_from_email, crate_cc_email

--- apps/crate_it/lib/mailer.php
+++ apps/crate_it/lib/mailer.php
@@ -5,19 +5,24 @@ namespace OCA\crate_it\lib;
 class Mailer {
   
   public function send($to, $from, $subject, $content) {
-    $result = (mail($to, $subject, $content, "From: $from\n"));
-    if(!$result) {
-      throw new Exception('Unable to send email at this time');
-    }
+    $result = mail($to, $subject, $content, "From: " . $from,  '-f ' . $from);
+  	if(!$result) {
+  		\OCP\Util::writeLog('Unable to send email: ', $result, \OCP\Util::ERROR);
+  	}
     return $result;
   }
 
-  public function sendHtml($to, $from, $subject, $content) {
-    $headers= "From: $from\n";
-    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-    $result = (mail($to, $subject, $content, $headers));
+  public function sendHtml($to, $cc, $from, $subject, $content) {
+  	if (!$from) {
+  		$from = 'noreply@crateit.app';
+  	}
+  	$headers = "From: " . $from . "\r\n";
+  	$headers = "Cc: " . $cc . "\r\n";
+  	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+    $result = mail($to, $subject, $content, $headers, '-f ' . $from);
     if(!$result) {
-      throw new Exception('Unable to send email at this time');
+    	\OCP\Util::writeLog('Unable to send email: ', $result, \OCP\Util::ERROR);
+    	\OCP\Util::writeLog('Headers: ', $headers, \OCP\Util::ERROR);
     }
     return $result;
   }
