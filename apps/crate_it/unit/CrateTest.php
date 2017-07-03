58092914b4c0f27b8336e967f8039910f6b3b44f
Wesley Zhang <wesley@digitalbridge.com.au>
2017-07-03 12:38:58 +1000

    more PSR2 code formatting

--- apps/crate_it/unit/CrateTest.php
+++ apps/crate_it/unit/CrateTest.php
@@ -7,11 +7,11 @@ require_once 'lib/util.php';
 
 use OCA\crate_it\lib\Crate;
 
-class CrateTest extends PHPUnit_Framework_TestCase {
-
-    public function testGetCrateFiles() {
+class CrateTest extends PHPUnit_Framework_TestCase
+{
+    public function testGetCrateFiles()
+    {
         $crate = new Crate('/tmp', 'Crate A', 'Description for Crate A');
         $this->assertNotNull($crate);
     }
 }
-
